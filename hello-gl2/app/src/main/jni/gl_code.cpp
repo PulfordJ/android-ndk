5b15bea68d2d2290e3ab079f89f758104d8e518f
John Pulford <johnpulford@gmail.com>
2016-02-19 00:28:49 +0000

    Textured cube.

--- hello-gl2/app/src/main/jni/gl_code.cpp
+++ hello-gl2/app/src/main/jni/gl_code.cpp
@@ -182,17 +182,25 @@ static void checkGlError(const char* op) {
     }
 }
 
-static const char gVertexShader[] = 
-    "attribute vec4 vPosition;\n"
-    "void main() {\n"
-    "  gl_Position = vPosition;\n"
-    "}\n";
-
-static const char gFragmentShader[] = 
-    "precision mediump float;\n"
-    "void main() {\n"
-    "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
-    "}\n";
+static const char glVertexShader[] =
+        "attribute vec4 vertexPosition;\n"
+        "attribute vec2 vertexTextureCord;\n"
+        "varying vec2 textureCord;\n"
+        "uniform mat4 projection;\n"
+        "uniform mat4 modelView;\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = projection * modelView * vertexPosition;\n"
+        "    textureCord = vertexTextureCord;\n"
+        "}\n";
+static const char glFragmentShader[] =
+        "precision mediump float;\n"
+        "uniform sampler2D texture;\n"
+        "varying vec2 textureCord;\n"
+        "void main()\n"
+        "{\n"
+        "    gl_FragColor = texture2D(texture, textureCord);\n"
+        "}\n";
 
 GLuint loadShader(GLenum shaderType, const char* pSource) {
     GLuint shader = glCreateShader(shaderType);
@@ -267,53 +275,77 @@ GLuint createProgram(const char* pVertexSource, const char* pFragmentSource) {
 GLuint simpleCubeProgram;
 GLuint gvPositionHandle;
 
-
-static const char  glVertexShader[] =
-        "attribute vec4 vertexPosition;\n"
-                "attribute vec3 vertexColour;\n"
-                "varying vec3 fragColour;\n"
-                "uniform mat4 projection;\n"
-                "uniform mat4 modelView;\n"
-                "void main()\n"
-                "{\n"
-                "    gl_Position = projection * modelView * vertexPosition;\n"
-                "    fragColour = vertexColour;\n"
-                "}\n";
-
-static const char  glFragmentShader[] =
-        "precision mediump float;\n"
-                "varying vec3 fragColour;\n"
-                "void main()\n"
-                "{\n"
-                "    gl_FragColor = vec4(fragColour, 1.0);\n"
-                "}\n";
-
 GLint vertexLocation;
+GLint textureCordLocation;
 GLint vertexColourLocation;
 GLint projectionLocation;
 GLint modelViewLocation;
+GLint samplerLocation;
+GLuint textureId;
 
 float projectionMatrix[16];
 
+GLuint loadSimpleTexture()
+{
+    /* Texture Object Handle. */
+    GLuint textureId;
+    /* 3 x 3 Image,  R G B A Channels RAW Format. */
+    GLubyte pixels[9 * 4] =
+            {
+                    18,  140, 171, 255, /* Some Colour Bottom Left. */
+                    143, 143, 143, 255, /* Some Colour Bottom Middle. */
+                    255, 255, 255, 255, /* Some Colour Bottom Right. */
+                    255, 255, 0,   255, /* Yellow Middle Left. */
+                    0,   255, 255, 255, /* Some Colour Middle. */
+                    255, 0,   255, 255, /* Some Colour Middle Right. */
+                    255, 0,   0,   255, /* Red Top Left. */
+                    0,   255, 0,   255, /* Green Top Middle. */
+                    0,   0,   255, 255, /* Blue Top Right. */
+            };
+    /* Use tightly packed data. */
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    /* Generate a texture object. */
+    glGenTextures(1, &textureId);
+    /* Activate a texture. */
+    glActiveTexture(GL_TEXTURE0);
+    /* Bind the texture object. */
+    glBindTexture(GL_TEXTURE_2D, textureId);
+    /* Load the texture. */
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 3, 3, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+    /* Set the filtering mode. */
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    return textureId;
+}
+
 bool setupGraphics(int width, int height)
 {
-    LOGI("Hello From the Native Side!! - setup Graphics - start");
     simpleCubeProgram = createProgram(glVertexShader, glFragmentShader);
-    if (simpleCubeProgram == 0)
+    if (!simpleCubeProgram)
     {
         LOGE ("Could not create program");
         return false;
     }
     vertexLocation = glGetAttribLocation(simpleCubeProgram, "vertexPosition");
-    vertexColourLocation = glGetAttribLocation(simpleCubeProgram, "vertexColour");
+    textureCordLocation = glGetAttribLocation(simpleCubeProgram, "vertexTextureCord");
     projectionLocation = glGetUniformLocation(simpleCubeProgram, "projection");
     modelViewLocation = glGetUniformLocation(simpleCubeProgram, "modelView");
-    /* Setup the perspective */
+    samplerLocation = glGetUniformLocation(simpleCubeProgram, "texture");
+    /* Setup the perspective. */
     matrixPerspective(projectionMatrix, 45, (float)width / (float)height, 0.1f, 100);
     glEnable(GL_DEPTH_TEST);
     glViewport(0, 0, width, height);
-    LOGI("Hello From the Native Side!! - setup Graphics");
-    return true;
+    /* Load the Texture. */
+    textureId = loadSimpleTexture();
+
+    if(textureId == 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
 
 GLfloat cubeVertices[] = {-1.0f,  1.0f, -1.0f, /* Back. */
@@ -332,40 +364,39 @@ GLfloat cubeVertices[] = {-1.0f,  1.0f, -1.0f, /* Back. */
                           1.0f, -1.0f, -1.0f,
                           1.0f, -1.0f,  1.0f,
                           1.0f,  1.0f,  1.0f,
-                          -1.0f, -1.0f, -1.0f, /* Top. */
-                          -1.0f, -1.0f,  1.0f,
-                          1.0f, -1.0f,  1.0f,
-                          1.0f, -1.0f, -1.0f,
-                          -1.0f,  1.0f, -1.0f, /* Bottom. */
-                          -1.0f,  1.0f,  1.0f,
-                          1.0f,  1.0f,  1.0f,
-                          1.0f,  1.0f, -1.0f
+                          -1.0f, 1.0f, -1.0f, /* Top. */
+                          -1.0f, 1.0f,  1.0f,
+                          1.0f, 1.0f,  1.0f,
+                          1.0f, 1.0f, -1.0f,
+                          -1.0f, - 1.0f, -1.0f, /* Bottom. */
+                          -1.0f,  -1.0f,  1.0f,
+                          1.0f, - 1.0f,  1.0f,
+                          1.0f,  -1.0f, -1.0f
 };
-
-GLfloat colour[] = {1.0f, 0.0f, 0.0f,
-                    1.0f, 0.0f, 0.0f,
-                    1.0f, 0.0f, 0.0f,
-                    1.0f, 0.0f, 0.0f,
-                    0.0f, 1.0f, 0.0f,
-                    0.0f, 1.0f, 0.0f,
-                    0.0f, 1.0f, 0.0f,
-                    0.0f, 1.0f, 0.0f,
-                    0.0f, 0.0f, 1.0f,
-                    0.0f, 0.0f, 1.0f,
-                    0.0f, 0.0f, 1.0f,
-                    0.0f, 0.0f, 1.0f,
-                    1.0f, 1.0f, 0.0f,
-                    1.0f, 1.0f, 0.0f,
-                    1.0f, 1.0f, 0.0f,
-                    1.0f, 1.0f, 0.0f,
-                    0.0f, 1.0f, 1.0f,
-                    0.0f, 1.0f, 1.0f,
-                    0.0f, 1.0f, 1.0f,
-                    0.0f, 1.0f, 1.0f,
-                    1.0f, 0.0f, 1.0f,
-                    1.0f, 0.0f, 1.0f,
-                    1.0f, 0.0f, 1.0f,
-                    1.0f, 0.0f, 1.0f
+GLfloat textureCords[] = { 1.0f, 1.0f, /* Back. */
+                           0.0f, 1.0f,
+                           1.0f, 0.0f,
+                           0.0f, 0.0f,
+                           0.0f, 1.0f, /* Front. */
+                           1.0f, 1.0f,
+                           0.0f, 0.0f,
+                           1.0f, 0.0f,
+                           0.0f, 1.0f, /* Left. */
+                           0.0f, 0.0f,
+                           1.0f, 0.0f,
+                           1.0f, 1.0f,
+                           1.0f, 1.0f, /* Right. */
+                           1.0f, 0.0f,
+                           0.0f, 0.0f,
+                           0.0f, 1.0f,
+                           0.0f, 1.0f, /* Top. */
+                           0.0f, 0.0f,
+                           1.0f, 0.0f,
+                           1.0f, 1.0f,
+                           0.0f, 0.0f, /* Bottom. */
+                           0.0f, 1.0f,
+                           1.0f, 1.0f,
+                           1.0f, 0.0f
 };
 
 GLushort indices[] = {0, 2, 3, 0, 1, 3, 4, 6, 7, 4, 5, 7, 8, 9, 10, 11, 8, 10, 12, 13, 14, 15, 12, 14, 16, 17, 18, 16, 19, 18, 20, 21, 22, 20, 23, 22};
@@ -383,11 +414,22 @@ void renderFrame()
     glUseProgram(simpleCubeProgram);
     glVertexAttribPointer(vertexLocation, 3, GL_FLOAT, GL_FALSE, 0, cubeVertices);
     glEnableVertexAttribArray(vertexLocation);
+
+    /*
     glVertexAttribPointer(vertexColourLocation, 3, GL_FLOAT, GL_FALSE, 0, colour);
     glEnableVertexAttribArray(vertexColourLocation);
     glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, projectionMatrix);
     glUniformMatrix4fv(modelViewLocation, 1, GL_FALSE, modelViewMatrix);
     glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, indices);
+     */
+
+    glVertexAttribPointer(textureCordLocation, 2, GL_FLOAT, GL_FALSE, 0, textureCords);
+    glEnableVertexAttribArray(textureCordLocation);
+    glUniformMatrix4fv(projectionLocation, 1, GL_FALSE,projectionMatrix);
+    glUniformMatrix4fv(modelViewLocation, 1, GL_FALSE, modelViewMatrix);
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, indices);
+    /* Set the sampler texture unit to 0. */
+    glUniform1i(samplerLocation, 0);
 
     angle += 1;
     if (angle > 360)
@@ -396,6 +438,11 @@ void renderFrame()
     }
     LOGI("Hello From the Native Side!! - render frame");
 }
+
+#include <GLES2/gl2ext.h>
+#include <cstdio>
+#include <cstdlib>
+
 
 extern "C" {
     JNIEXPORT void JNICALL Java_com_android_gl2jni_GL2JNILib_init(JNIEnv * env, jobject obj,  jint width, jint height);
